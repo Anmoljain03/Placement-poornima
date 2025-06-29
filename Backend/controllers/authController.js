fc0660ea34d8893df5400d9d242ad9b6580adc72
Anmol Jain <128278782+Anmoljain03@users.noreply.github.com>
2025-06-29 16:46:02 +0530

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const User = require("../models/User");
 const OTP = require("../models/otpModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
